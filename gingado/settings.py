94983442e214f6019b0fe27806467a54dd00841e
robin-mader-bis <128372203+robin-mader-bis@users.noreply.github.com>
2024-03-08 09:46:20 +0100

    Feature: Add load_CB_speeches function (#12)
    * Feature: Add load_CB_speeches function
    
    * Fix: Add requests as an explicit dependency
    
    * Fix: Add missing context for opening file
    
    * Doc: Add additional citation and BIS website links
    
    * Doc: Add updated HTML file for datasets documentation
    
    * Fix: Add future import for type hinting compatibility with Python 3.9

---
+++ gingado/settings.py
@@ -0,0 +1,17 @@
+"""Settings for the gingado library."""
+
+## GENERAL SETTINGS
+
+# Path to the directory cached datasets are stored in
+CACHE_DIRECTORY = './gingado'
+
+## CB SPEECHES SETTINGS
+
+# Base URL of CB speeches files (should end in a slash)
+CB_SPEECHES_BASE_URL = 'https://www.bis.org/speeches/'
+
+# Base name of the zip files on the website, used to create the full URL for downloading the file
+CB_SPEECHES_ZIP_BASE_FILENAME = 'speeches'
+
+# Base path used for storing the speeches files on disk
+CB_SPEECHES_CSV_BASE_FILENAME = 'cb_speeches'

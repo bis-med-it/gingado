bffde8a0a89ffca22cfb2d516ff10d8ac6e0ff7a
Douglas Araujo <dkg.araujo@gmail.com>
2025-04-25 17:20:33 +0200

    Update to setup.py and manifest

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import setuptools
 import re
 
+
 # Function to read requirements from a file and return a list
 def read_requirements(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return [line.strip() for line in file.readlines()]
+
 
 def get_version_from_init() -> str:
     """
@@ -21,38 +23,38 @@ def get_version_from_init() -> str:
 
 
 # Parse requirements
-requirements = read_requirements('requirements.txt')
-dev_requirements = read_requirements('dev_requirements.txt')
+requirements = read_requirements("requirements.txt")
+dev_requirements = read_requirements("dev_requirements.txt")
 
 setuptools.setup(
-    name='gingado',
+    name="gingado",
     version=get_version_from_init(),
-    description='A machine learning library for economics and finance',
-    long_description=open('README.md', encoding='utf8').read(),
-    long_description_content_type='text/markdown',
-    author='Douglas K. G. de Araujo',
-    author_email='Douglas.Araujo@bis.org',
-    license='Apache Software License 2.0',
+    description="A machine learning library for economics and finance",
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    author="Douglas K. G. de Araujo",
+    author_email="Douglas.Araujo@bis.org",
+    license="Apache-2.0",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
-    url='https://github.com/bis-med-it/gingado',
+    url="https://github.com/bis-med-it/gingado",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    extras_require={
-        'dev': dev_requirements
-    },
-    python_requires='>=3.7',
+    extras_require={"dev": dev_requirements},
+    python_requires=">=3.7",
     zip_safe=False,
     entry_points={},
 )
-
